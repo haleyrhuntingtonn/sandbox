17e90e5d4bdc6fa78592a66b19a26646faa1c341
Haley Huntington <haleyrhuntington@gmail.com>
2020-04-10 15:33:02 -0400

    Tool bar

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const toolbar = props => (
+    <header>
+        <nav> 
+            <div></div>
+            <div><a href="/">THE LOGO</a></div>
+            <div>
+                <ul>
+                    <li><a href="/">Products</a></li>
+                    <li><a href="/">Users</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+export default toolbar;
